d3a8efde2b1a4c1105b6ec98a25773a693a1e209
Jetross <jetrossneri07@gmail.com>
2025-11-01 00:08:46 +0800

    removed the navbar

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,9 +1,9 @@
-import Header from "./components/Header";
+import { Navbar } from "./components/Navbar";
 
 export default function LandingPage() {
   return (
     <div className="bg-white min-h-screen">
-      <Header />
+      <Navbar />
     </div>
   );
 }
